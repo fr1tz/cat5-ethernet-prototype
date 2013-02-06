bd77cfa7ddddfa5513ffc677b8cfd155a234926a
Michael Goldener <mg@wasted.ch>
2013-02-06 14:51:23 +0100

    Add function clientCmdHud().

--- game/client/scripts/cmds.cs
+++ game/client/scripts/cmds.cs
@@ -20,8 +20,42 @@ function clientCmdSkyColor(%color, %elementMask)
    }
 }
 
-
 function clientCmdCrosshair(%option, %arg1, %arg2, %arg3, %arg4, %arg5)
 {
+   //error("clientCmdCrosshair():" SPC %arg1 SPC %arg2 SPC %arg3 SPC %arg4 SPC %arg5);
    Hud.setCrosshair(%option, %arg1, %arg2, %arg3, %arg4, %arg5);
+}
+
+function clientCmdHud(%option, %arg1, %arg2, %arg3, %arg4, %arg5)
+{
+   //error("clientCmdHud():" SPC %arg1 SPC %arg2 SPC %arg3 SPC %arg4 SPC %arg5);
+   if(%option $= "health")
+   {
+      if(%arg1 !$= "")
+      {
+         HealthMeter.setVisible(%arg1);
+         HealthIcon.setVisible(%arg1);
+      }
+   }
+   else if(%option $= "energy")
+   {
+      if(%arg1 !$= "")
+         EnergyMeter.setVisible(%arg1);
+      if(%arg2 !$= "")
+      {
+         EnergyMeter.bitmap = %arg2;
+         Hud.remove(EnergyMeter);
+         Hud.add(EnergyMeter);
+      }
+   }
+   else if(%option $= "minimap")
+   {
+      if(%arg1 !$= "")
+         MiniMap.setVisible(%arg1);
+      if(%arg2 !$= "")
+         MiniMap.visRadius = %arg2;
+      if(%arg3 !$= "")
+         MiniMap.rotate = %arg3;
+
+   }
 }
