3d613ce0ce9476adfa6eb2290af6c51320520468
Michael Goldener <mg@wasted.ch>
2013-02-19 05:29:20 +0100

    Add custom etherform: fr1tz.

---
+++ game/server/etherforms/fr1tz.cs
@@ -0,0 +1,9 @@
+//------------------------------------------------------------------------------
+// Cat5
+// Copyright notices are in the file named COPYING.
+//------------------------------------------------------------------------------
+
+datablock EtherformData(EthFr1tz : EthStandard)
+{
+	shapeFile = "share/shapes/cat5/etherforms/fr1tz.dts";
+};
