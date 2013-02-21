376bdf9ab8967aeca75cf0ad316f2254ce14ff02
Michael Goldener <mg@wasted.ch>
2013-02-21 05:40:20 +0100

    Add pumpgunner spawn explosion.

---
+++ game/server/forms/pumpgunner/pumpgunner.spawnfx.cs
@@ -0,0 +1,202 @@
+//------------------------------------------------------------------------------
+// Cat5
+// Copyright notices are in the file named COPYING.
+//------------------------------------------------------------------------------
+
+datablock ParticleData(PumpgunnerSpawnExplosion_Cloud)
+{
+	dragCoeffiecient	  = 0.4;
+	gravityCoefficient	= 0;
+	inheritedVelFactor	= 0.025;
+
+	lifetimeMS			  = 1000;
+	lifetimeVarianceMS	= 0;
+
+	useInvAlpha =  false;
+	spinRandomMin = -200.0;
+	spinRandomMax =  200.0;
+
+	textureName			 = "share/textures/cat5/star3.white.png";
+
+	colors[0]	  = "1.0 1.0 1.0 1.0";
+	colors[1]	  = "0.0 0.0 1.0 0.0";
+	colors[2]	  = "0.0 0.0 1.0 0.0";
+	sizes[0]		= 12.0;
+	sizes[1]		= 9.0;
+	sizes[2]		= 0.0;
+	times[0]		= 0.0;
+	times[1]		= 0.25;
+	times[2]		= 1.0;
+
+	allowLighting = false;
+};
+
+datablock ParticleEmitterData(PumpgunnerSpawnExplosion_CloudEmitter)
+{
+	ejectionPeriodMS = 5;
+	periodVarianceMS = 0;
+
+	ejectionVelocity = 6.25;
+	velocityVariance = 0.25;
+
+	thetaMin			= 0.0;
+	thetaMax			= 90.0;
+
+	lifetimeMS		 = 100;
+
+	particles = "PumpgunnerSpawnExplosion_Cloud";
+};
+
+datablock ParticleData(PumpgunnerSpawnExplosion_Dust)
+{
+	dragCoefficient		= 1.0;
+	gravityCoefficient	= -0.01;
+	inheritedVelFactor	= 0.0;
+	constantAcceleration = 0.0;
+	lifetimeMS			  = 1000;
+	lifetimeVarianceMS	= 100;
+	useInvAlpha			 = true;
+	spinRandomMin		  = -90.0;
+	spinRandomMax		  = 500.0;
+	textureName			 = "share/textures/cat5/star3.white.png";
+	colors[0]	  = "0.9 0.9 0.9 0.5";
+	colors[1]	  = "0.9 0.9 0.9 0.5";
+	colors[2]	  = "0.9 0.9 0.9 0.0";
+	sizes[0]		= 3.2;
+	sizes[1]		= 4.6;
+	sizes[2]		= 5.0;
+	times[0]		= 0.0;
+	times[1]		= 0.7;
+	times[2]		= 1.0;
+	allowLighting = true;
+};
+
+datablock ParticleEmitterData(PumpgunnerSpawnExplosion_DustEmitter)
+{
+	ejectionPeriodMS = 5;
+	periodVarianceMS = 0;
+	ejectionVelocity = 15.0;
+	velocityVariance = 0.0;
+	ejectionOffset	= 0.0;
+	thetaMin			= 90;
+	thetaMax			= 90;
+	phiReferenceVel  = 0;
+	phiVariance		= 360;
+	overrideAdvances = false;
+	lifetimeMS		 = 250;
+	particles = "PumpgunnerSpawnExplosion_Dust";
+};
+
+
+datablock ParticleData(PumpgunnerSpawnExplosion_Smoke)
+{
+	dragCoeffiecient	  = 0.4;
+	gravityCoefficient	= -0.5;	// rises slowly
+	inheritedVelFactor	= 0.025;
+
+	lifetimeMS			  = 1250;
+	lifetimeVarianceMS	= 0;
+
+	useInvAlpha =  true;
+	spinRandomMin = -200.0;
+	spinRandomMax =  200.0;
+
+	textureName			 = "share/textures/cat5/star3.white.png";
+
+	colors[0]	  = "0.9 0.9 0.9 0.4";
+	colors[1]	  = "0.9 0.9 0.9 0.2";
+	colors[2]	  = "0.9 0.9 0.9 0.0";
+	sizes[0]		= 2.0;
+	sizes[1]		= 6.0;
+	sizes[2]		= 2.0;
+	times[0]		= 0.0;
+	times[1]		= 0.5;
+	times[2]		= 1.0;
+
+	allowLighting = true;
+};
+
+datablock ParticleEmitterData(PumpgunnerSpawnExplosion_SmokeEmitter)
+{
+	ejectionPeriodMS = 5;
+	periodVarianceMS = 0;
+
+	ejectionVelocity = 6.25;
+	velocityVariance = 0.25;
+
+	thetaMin			= 0.0;
+	thetaMax			= 180.0;
+
+	lifetimeMS		 = 250;
+
+	particles = "PumpgunnerSpawnExplosion_Smoke";
+};
+
+datablock ParticleData(PumpgunnerSpawnExplosion_Sparks)
+{
+	dragCoefficient		= 1;
+	gravityCoefficient	= 0.0;
+	inheritedVelFactor	= 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS			  = 500;
+	lifetimeVarianceMS	= 350;
+	textureName			 = "share/textures/cat5/star3.white.png";
+	colors[0]	  = "0.56 0.36 0.26 1.0";
+	colors[1]	  = "0.56 0.36 0.26 1.0";
+	colors[2]	  = "1.0 0.36 0.26 0.0";
+	sizes[0]		= 0.5;
+	sizes[1]		= 0.5;
+	sizes[2]		= 0.75;
+	times[0]		= 0.0;
+	times[1]		= 0.5;
+	times[2]		= 1.0;
+	allowLighting = false;
+};
+
+datablock ParticleEmitterData(PumpgunnerSpawnExplosion_SparksEmitter)
+{
+	ejectionPeriodMS = 2;
+	periodVarianceMS = 0;
+	ejectionVelocity = 12;
+	velocityVariance = 6.75;
+	ejectionOffset	= 0.0;
+	thetaMin			= 0;
+	thetaMax			= 60;
+	phiReferenceVel  = 0;
+	phiVariance		= 360;
+	overrideAdvances = false;
+	orientParticles  = true;
+	lifetimeMS		 = 100;
+	particles = "PumpgunnerSpawnExplosion_Sparks";
+};
+
+datablock ExplosionData(PumpgunnerSpawnExplosion)
+{
+	soundProfile = CatSpawnSound;
+
+	faceViewer	  = true;
+	explosionScale = "0.8 0.8 0.8";
+
+	lifetimeMS = 200;
+
+	particleEmitter = PumpgunnerSpawnExplosion_CloudEmitter;
+	particleDensity = 50;
+	particleRadius = 1;
+
+	//emitter[0] = PumpgunnerSpawnExplosion_DustEmitter;
+	//emitter[1] = PumpgunnerSpawnExplosion_SmokeEmitter;
+	//emitter[2] = PumpgunnerSpawnExplosion_SparksEmitter;
+
+	// Camera shake
+	shakeCamera = false;
+	camShakeFreq = "10.0 6.0 9.0";
+	camShakeAmp = "20.0 20.0 20.0";
+	camShakeDuration = 0.5;
+	camShakeRadius = 20.0;
+
+	// Dynamic light
+	//lightStartRadius = 15;
+	//lightEndRadius = 0;
+	//lightStartColor = "1.0 0.0 0.0";
+	//lightEndColor = "0.0 0.0 0.0";
+};
