fda21bbba49f7197cc85f0ab61f07827c8ef49b2
Michael Goldener <mg@wasted.ch>
2013-03-01 15:11:03 +0100

    Add rain & waterblock to test1 map.

--- game/arenas/cat5-ethernet/test1/mission.env.cs
+++ game/arenas/cat5-ethernet/test1/mission.env.cs
@@ -14,11 +14,26 @@ sgLoadDataBlocks($sgLightEditor::filterDBPath);
 // Material mappings
 //------------------------------------------------------------------------------
 
-%mapping = createMaterialMapping("dark_grey_blue_grid");
-%mapping.sound = $MaterialMapping::Sound::Metal;
-%mapping.color = "0 1 0 1.0 0.0";
+//%mapping = createMaterialMapping("dark_grey_blue_grid");
+//%mapping.sound = $MaterialMapping::Sound::Metal;
+//%mapping.color = "0 1 0 1.0 0.0";
 
-%mapping = createMaterialMapping("malloc/dark_blue_grid");
-%mapping.sound = $MaterialMapping::Sound::Metal;
-%mapping.color = "0 1 0 1.0 0.0";
+//%mapping = createMaterialMapping("malloc/dark_blue_grid");
+//%mapping.sound = $MaterialMapping::Sound::Metal;
+//%mapping.color = "0 1 0 1.0 0.0";
 //%mapping.envmap = "share/textures/malloc/dark_blue_grid 0.5";
+
+//------------------------------------------------------------------------------
+// Precipitation
+//------------------------------------------------------------------------------
+
+datablock PrecipitationData(MissionRain)
+{
+   dropTexture = "share/textures/cat5-testmap1/raindrops";
+   //splashTexture = "share/textures/cat5-testmap1/raindrops.splash";
+   dropSize = 0.40;
+   //splashSize = 0.2;
+   useTrueBillboards = false;
+   splashMS = 200;
+};
+
